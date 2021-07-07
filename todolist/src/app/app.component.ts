b6ed319a65db85c2041b8e9e9e7fff412b5eb02a
Thamires Telles <thamirestelles12@gmail.com>
2021-07-07 12:43:33 -0300

    .

--- todolist/src/app/app.component.ts
+++ todolist/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolist';
+
+  public itens = ['item 1', 'item 2', 'item 3', 'item 4'];
+
+  
+  
 }
